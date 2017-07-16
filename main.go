7c13db5620573b170859ff1f482a46f27dc60d7c
diamonddelt <rrrasti@yahoo.com>
2017-07-16 13:20:48 -0500

    Add support for .ini

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/go-ini/ini"
+
 	"gopkg.in/yaml.v2"
 )
 
@@ -23,6 +25,22 @@ type YAMLConfiguration struct {
 	Language string         `yaml:"language"`
 	Skills   map[string]int `yaml:"skills"`
 	Active   bool           `yaml:"active"`
+}
+
+// INIConfiguration specifies how to map this project-specific Windows INI file to a struct
+type INIConfiguration struct {
+	Name     string `ini:"name"`
+	Language string `ini:"language"`
+	Skills   struct {
+		Golang   int `ini:"golang"`
+		Python   int `ini:"python"`
+		Java     int `ini:"java"`
+		CSharp   int `ini:"csharp"`
+		Training int `ini:"training"`
+	}
+	State struct {
+		Active bool `ini:"active"`
+	}
 }
 
 // DecodeJSON parses a JSON configuration specified by the 'JSONConfiguration' struct
@@ -51,6 +69,15 @@ func UnmarshalYAML(config *YAMLConfiguration, path string) {
 	}
 }
 
+// MapINI maps the current configuration of an .ini file to the config object reference at the specified path
+func MapINI(config *INIConfiguration, path string) {
+	err := ini.MapTo(&config, path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	configJSON := JSONConfiguration{}
 	DecodeJSON(&configJSON, "config.json")
@@ -59,4 +86,8 @@ func main() {
 	configYAML := YAMLConfiguration{}
 	UnmarshalYAML(&configYAML, "config.yaml")
 	fmt.Println(`The value for "skills: training" is:`, configYAML.Skills["training"]) // pull out a value from the YAML configuration file
+
+	configINI := INIConfiguration{}
+	MapINI(&configINI, "config.ini")
+	fmt.Println(`The value for "active" in the [State] section is:`, configINI.State.Active) // pull out a value from the INI configuration file
 }
