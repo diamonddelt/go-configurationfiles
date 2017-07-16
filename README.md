8cde9ed2834e8b0168465cccaaa830737613ddbc
diamonddelt <rrrasti@yahoo.com>
2017-07-16 07:33:00 -0500

    Add YAML configuration parsing

---
+++ README.md
@@ -0,0 +1,6 @@
+## This repository showcases a few examples of how Go can be used to parse data from common configuration file formats.
+### Formats with examples provided:
+### - JSON
+### - YAML
+
+## Ryan Rasti rrrasti@yahoo.com
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
-// JSONConfiguration specifies how to unmarshal a given configuration file
+// JSONConfiguration specifies how to unmarshal a given JSON configuration file
 type JSONConfiguration struct {
 	Name     string                 `json:"name"`
 	Language string                 `json:"language"`
@@ -14,9 +17,17 @@ type JSONConfiguration struct {
 	Active   bool                   `json:"active"`
 }
 
-// UnmarshalJSON parses a JSON configuration specified by the 'JSONConfiguration' struct
-func UnmarshalJSON(config *JSONConfiguration) {
-	file, _ := os.Open("config.json")
+// YAMLConfiguration specifies how to unmarshal a given YAML configuration file
+type YAMLConfiguration struct {
+	Name     string         `yaml:"name"`
+	Language string         `yaml:"language"`
+	Skills   map[string]int `yaml:"skills"`
+	Active   bool           `yaml:"active"`
+}
+
+// DecodeJSON parses a JSON configuration specified by the 'JSONConfiguration' struct
+func DecodeJSON(config *JSONConfiguration, path string) {
+	file, _ := os.Open(path)
 	defer file.Close()
 
 	decoder := json.NewDecoder(file)
@@ -27,9 +38,25 @@ func UnmarshalJSON(config *JSONConfiguration) {
 	}
 }
 
+// UnmarshalYAML unmarshalls a YAML configuration into a struct for logical key/value access
+func UnmarshalYAML(config *YAMLConfiguration, path string) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(data, config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
-	config := JSONConfiguration{}
-	UnmarshalJSON(&config)
-	fmt.Println(config)
-	fmt.Println(config.Skills["Golang"]) // pull out a value from the configuration file
+	configJSON := JSONConfiguration{}
+	DecodeJSON(&configJSON, "config.json")
+	fmt.Println(`The value for "skills: golang" is:`, configJSON.Skills["golang"]) // pull out a value from the JSON configuration file
+
+	configYAML := YAMLConfiguration{}
+	UnmarshalYAML(&configYAML, "config.yaml")
+	fmt.Println(`The value for "skills: training" is:`, configYAML.Skills["training"]) // pull out a value from the YAML configuration file
 }
